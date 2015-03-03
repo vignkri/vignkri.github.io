31dc0bff18f726b7ba33f1169d1945f81c537e10
Vignesh Krishnamoorthy <k.vignesh.krish@gmail.com>
2015-03-03 10:00:25 +0100

    small changes in reading_time plugin

--- _plugins/reading_time.rb
+++ _plugins/reading_time.rb
@@ -11,7 +11,7 @@ module ReadingTimeFilter
 		words = input.split.size;
 		minutes = ( words / words_per_minute ).floor
 		minutes_label = minutes === 1 ? " minute" : " minutes"
-		minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute"
+		minutes > 0 ? "About #{minutes} #{minutes_label}" : "less than 1 minute"
 	end
 end
 
